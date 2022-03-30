3922888f0b448aa4e6cb9b8cf5b159188cf77bb8
Aleksey <axcerz@gmail.com>
2022-03-30 19:15:49 +1000

    Store.upload

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,20 @@
-class Store {
+const APPLICATION_KEY = "__crm_app__";
 
+class Store {
+	_orders = [];
+
+	get orders() {
+		return JSON.parse(JSON.stringify(this._orders));
+	}
+
+	upload() {
+		const orders = JSON.stringify(this._orders);
+		localStorage.setItem(APPLICATION_KEY, orders);
+	}
+
+	download() {
+		
+	}
 }
 
 export default Store;
