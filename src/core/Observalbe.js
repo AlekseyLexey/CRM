64f35d8f5fb42112716429b8c000080c7ba719ab
Aleksey <axcerz@gmail.com>
2022-04-02 05:38:38 +1000

    Observable.js

---
+++ src/core/Observalbe.js
@@ -0,0 +1,26 @@
+class Observable {
+	subscribers = new Map();
+
+	subscribe(subscriber) {
+		const key = Symbol('key');
+		this.subscribers.set(key, subscriber);
+		console.log(this.subscribers);
+
+		return () => {
+			if (this.subscribers.has(key)) {
+				this.subscribers.delete(key);
+				return true;
+			}
+
+			return false;
+		};
+	}
+
+	dispatch(...data) {
+		for (const subscriber of this.subscribers.values()) {
+			subscriber(...data);
+		}
+	}
+}
+
+export default Observable;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import Store from "./Store.js";
+import Store from "./core/Store.js";
+import Observable from "./core/Observalbe.js";
 
 const store = new Store();
 store.download();
 store.upload();
+
+const ob = new Observable();
+
+ob.subscribe('sub1');
