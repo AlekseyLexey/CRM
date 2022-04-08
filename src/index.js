b946480b7efeebd869cb7073813209d32af0e7a8
Aleksey <axcerz@gmail.com>
2022-04-09 03:15:11 +1000

    day4 complete

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Store from "./core/Store.js";
 import Observable from "./core/Observalbe.js";
 import EventEmitter from "./core/EventEmitter.js";
 import OrdersTable from "./core/OrdersTable.js";
+import Paginator from "./core/Paginator.js";
 
 const store = new Store();
 store.download();
@@ -13,3 +14,7 @@ const ot = new OrdersTable(document.querySelector('[data-mount="ordersTable"]'),
 
 
 ot.on('edit', (id) => console.log(id));
+
+const pagination = new Paginator(document.querySelector('[data-mount="pagination"]'), 5, 1);
+
+pagination.on('move', (number) => console.log(number));
